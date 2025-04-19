6f7fff63e5eeda051935bc8332838a7d66871831
administroot <1474668090@qq.com>
2025-04-19 16:30:29 +0800

    :sparkles: `About Iced` Page

--- EtaDot/about_freeeta_scene.gd
+++ EtaDot/about_freeeta_scene.gd
@@ -1,16 +1,20 @@
 extends PopupPanel
 
 var item_contents = [
-	"Godot License\n这是Godot引擎的许可证信息...",
-	"Serde_json License\n这是Serde_json库的许可证信息...",
-	"Tokio License\n这是Tokio库的许可证信息...",
+	"Godot Engine\n\tFiles:\n\t\t*\n\t© 2014-present, Godot Engine contributors\n\t© 2007-2017, Juan Linietsky, Ariel Manzur\n\tLicense: Expat",
+	"Serde_json\n\tFiles:\n\t\trust/src/serial.rs\n\t© 2015-present, Serde contributors\n\tLicense: MIT OR Apache-2.0",
+	"Tokio\n\tFiles:\n\t\trust/src/driver.rs\n\t© 2016-present, Tokio-rs contributors\n\tLicense: MIT ",
 ]
 
 func _on_license_itemlist_item_selected(index: int) -> void:
 	var txt_label = $"WindowTypeSetting/SelectTab/Third-Party License/Third-PartyVContainer/LicenseHSplit/LicenseScroll/SpecificLicense"
-
+	txt_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	
 	if index >= 0 and index < item_contents.size():
-		print(item_contents[index])
 		txt_label.text = item_contents[index]
 	else:
 		push_error("No License found")
+
+
+func _on_confirm_button_pressed() -> void:
+	hide()
