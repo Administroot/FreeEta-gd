b4584874ae5fdb5ffee245c7623cf6fa5c07238e
administroot <1474668090@qq.com>
2025-04-21 17:36:27 +0800

    :sparkles: Automatically load `CommonNode`

--- EtaDot/common_node.gd
+++ EtaDot/common_node.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func _ready() -> void:
+	set_properties("res://assets/pump.svg")
+	
+func set_properties(picture: String) -> void:
+	$Body/photo.texture = load(picture)
+	var picture_size = $Body/photo.texture.get_size()
+	$Body/CollisionShape.shape.size = picture_size
+	$Body/Button.size = picture_size
+	$Body/Button.position = - picture_size / 2.
