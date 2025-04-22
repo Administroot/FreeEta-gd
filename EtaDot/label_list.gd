2262281c1abff4a3ad71d2fc6a4ef0bb8c0961b2
administroot <1474668090@qq.com>
2025-04-22 16:27:52 +0800

    :sparkles: Inteoduce `Label` tree in `CreateNodeScene`

---
+++ EtaDot/label_list.gd
@@ -0,0 +1,18 @@
+extends Tree
+
+func _ready() -> void:
+	var root = create_item()
+	create_label(root, "red", "res://assets/redpoint.svg")
+	create_label(root, "orange", "res://assets/orangepoint.svg")
+	create_label(root, "yellow", "res://assets/yellowpoint.svg")
+	create_label(root, "green", "res://assets/greenpoint.svg")
+	create_label(root, "blue", "res://assets/bluepoint.svg")
+	create_label(root, "purple", "res://assets/purplepoint.svg")
+	create_label(root, "grey", "res://assets/greypoint.svg")
+	
+func create_label(parent: TreeItem, label_name: String, path: String) -> void:
+	var label = create_item(parent)
+	label.set_text(0, label_name)
+	label.set_custom_font_size(0, 25)
+	label.add_button(0, load(path), -1, false, "")
+	label.set_editable(0, true)
--- EtaDot/top_bar.gd
+++ EtaDot/top_bar.gd
@@ -4,3 +4,8 @@ func _on_help_menu_id_pressed(id: int) -> void:
 	match id:
 		# 🌏 About FreeEta
 		1: $HelpMenu/AboutFreeetaScene.popup_centered()
+
+func _on_graphics_menu_id_pressed(id: int) -> void:
+	match id:
+		# ➕ Add More ...
+		2: $GraphicsMenu/CreateNodeScene.popup_centered()
