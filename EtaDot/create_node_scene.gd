12f08ac7de752b4ecf61ecb33510ac60893fde57
administroot <1474668090@qq.com>
2025-05-02 15:25:01 +0800

    :sparkles: Add TreeItem logic

--- EtaDot/create_node_scene.gd
+++ EtaDot/create_node_scene.gd
@@ -4,13 +4,14 @@ extends PopupPanel
 @onready var node_list = $"HSplit1/NodeBox/NodeList"
 @onready var desc_box = $"HSplit1/NodeBox/DescBox"
 
+
 func _ready() -> void:
 	
 	var root = node_list.create_item()
 	for i in range(len(data.types)):
 		var node = data.get_type(i)
 		create_label(root, node)
-	
+
 func create_label(parent: TreeItem, node: NodeType) -> void:
 	var label = node_list.create_item(parent)
 	label.set_text(0, node.node_name)
@@ -32,8 +33,20 @@ func _on_node_list_cell_selected() -> void:
 	else :
 		LogUtil.error("Node data not found in Json")
 		push_error("Node data not found in Json")
-	
 
+# Submit and add a `TreeItem`
+#region Add Node
+func _on_add_node_line_edit_text_submitted(new_text: String) -> void:
+	for node_name in TreeUtil.get_column_item_names(node_list):
+		if node_name == new_text:
+			# TODO: Pop a warning panel here
+			LogUtil.warning("Your new NodeType Name %s is duplicated with the existing node name. Are you sure to continue?" % new_text)
+		else :
+			# TODO: Pop a add node panel here.
+			# And clear the text
+			$"HSplit1/NodeBox/HBox1/AddNodeLineEdit".clear()
+
+#region JSON Serialize and Deserialize
 func get_nodes() -> NodeTypes:
 	var loaded_data: NodeTypes = JsonClassConverter.json_file_to_class(NodeTypes, "user://saves/node_types.json") 
 	if !loaded_data:
---
+++ EtaDot/scripts/tree.gd
@@ -0,0 +1,15 @@
+class_name TreeUtil
+
+static func get_column_item_names(tree: Tree, column: int = 0) -> PackedStringArray:
+	var names: Array[String] = []
+	var root = tree.get_root()
+	if root:
+		_traverse_items(root, names, column)
+	return PackedStringArray(names)
+
+static func _traverse_items(item: TreeItem, names: Array[String], column: int) -> void:
+	names.append(item.get_text(column))
+	var child = item.get_first_child()
+	while child:
+		_traverse_items(child, names, column)
+		child = child.get_next()
