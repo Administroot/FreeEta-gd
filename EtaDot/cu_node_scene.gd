b491b7506b107e2ecf0c8cfa0d6bb870bb1da488
administroot <1474668090@qq.com>
2025-05-14 15:19:31 +0800

    :lipstick: Draw `CU_Node_Scene`

--- EtaDot/cu_node_scene.gd
+++ EtaDot/cu_node_scene.gd
@@ -1,6 +1,10 @@
 extends Panel
 
 # TODO: Draw this scene.
+# NodeName: String
+# NodeType: String
+# <Short Desc>
+# Reliability: float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
