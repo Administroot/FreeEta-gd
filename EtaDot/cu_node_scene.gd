294d5a2880504b9d790498d6b27decc18a58b967
administroot <1474668090@qq.com>
2025-05-27 17:34:29 +0800

    :necktie: Workaround: Add scroll function to ShowDesc `TextLabel`

--- EtaDot/cu_node_scene.gd
+++ EtaDot/cu_node_scene.gd
@@ -17,11 +17,10 @@ func _ready() -> void:
 			$VBox/Grid1/OptionButton.add_item(typename)
 	# Short Description
 	_on_option_button_item_selected($VBox/Grid1/OptionButton.selected)
-	# FIXME: Scale panel automatically when `short_desc` has multiple lines
 	# Reliability
 	$"VBox/Grid2/ReliEdit".text = str(component.reliability)
 
-
 func _on_option_button_item_selected(index: int) -> void:
 	var typename = $VBox/Grid1/OptionButton.get_item_text(index)
-	$VBox/ShowDesc.text = GlobalData.nodetypes_data.get_type_by_name(typename).short_desc
+	var showdesc = $VBox/ShowDesc
+	showdesc.text = GlobalData.nodetypes_data.get_type_by_name(typename).short_desc
