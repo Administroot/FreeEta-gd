427bc0504771fe3c5dbd2e32bfcde597677c2bed
administroot <1474668090@qq.com>
2025-07-24 19:20:05 +0800

    :sparkles: Connect `tmp_files_prepared` signal to `Calculator`

--- EtaDot/eta_scene.gd
+++ EtaDot/eta_scene.gd
@@ -1,1 +1,5 @@
 extends Node2D
+
+
+func _on_calculator_tmp_files_prepared() -> void:
+	LogUtil.info("Tmp files all prepared!")
--- rust/etalib/src/calc.rs
+++ rust/etalib/src/calc.rs
@@ -1,5 +1,6 @@
 use godot::classes::{file_access::ModeFlags, Node};
 use godot::prelude::*;
+use crate::common::algorithm;
 use crate::model::IData;
 
 #[derive(GodotClass)]
@@ -12,12 +13,19 @@ struct Calculator {
 
 use godot::classes::{INode, FileAccess};
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
+
+const TMP_OUTPUT_FILE: &str = "tmp_output.json";
 
 #[godot_api]
 impl INode for Calculator {
     fn init(base: Base<Node>) -> Self {
         Self { idata: IData::initialize("user://saves/components.json"), base }
+    }
+
+    fn ready(&mut self) {
+        self.generate_eta_data();
+        Self::remove_tmp_file(Path::new(TMP_OUTPUT_FILE));
     }
 }
 
@@ -25,27 +33,47 @@ impl INode for Calculator {
 impl Calculator {
     #[func]
     fn generate_eta_data(&mut self){
-        // TODO:
-        let _ = self;
-        godot_print!("I have got system!");
+        let odata = algorithm(&mut self.idata);
+        let path = Path::new(TMP_OUTPUT_FILE);
+        match odata.serialize(path) {
+            Ok(_) => {},
+            Err(e) => godot_error!("Could not buffer to {}: {}", path.display(), e),
+        };
+        self.signals().tmp_files_prepared().emit();
     }
+
+    fn remove_tmp_file(path: &Path){
+        match fs::remove_file(path) {
+            Ok(_) => {},
+            Err(e) => godot_error!("Remove tmp file {} failed: {}", path.display(), e),
+        }
+    }
+
+    #[signal]
+    fn tmp_files_prepared();
 }
 
 impl IData {
-    pub fn initialize(path: &str) -> Self {
+    fn initialize(path: &str) -> Self {
         let file = FileAccess::open(path, ModeFlags::READ)
             .ok_or("Failed to open save file").unwrap();
         let content = file.get_as_text();
-        // Write the contents to a temporary file so we can use deserialize_system_from_path
-        let tmp_path = PathBuf::from(&format!("tmp_{}" , &path));
-        fs::write(&tmp_path, content.to_string()).expect(&format!("Cannnot write buffer to {}", tmp_path.display()));
-        // Optionally, remove the temporary file
-        let _ = fs::remove_file(&tmp_path);
+
         let mut data = IData::new();
-        match data.deserialize(&tmp_path) {
+        match data.deserialize_gstring(&content) {
             Ok(_) => {},
             Err(e) => godot_error!("Deserialize stage failed: {}", e),
         }
         data
+    }
+
+    fn deserialize_gstring(&mut self, txt: &GString) -> Result<(), Box<dyn std::error::Error>> {
+        match serde_json::from_str::<IData>(&txt.to_string()) {
+            Ok(parsed) => {
+                *self = parsed;
+                Ok(())
+            }
+            Err(e) => Err(Box::new(e)),
+        }
     }
 }
